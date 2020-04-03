d320a2be153e1169abc19b59a81993339277aeec
anthonyl <anthony.lysiak@hotmail.com>
2020-04-03 13:23:03 +0100

    Le méchanisme de frappe (typing.js) commence a ressembler a quelque chose.

--- src/javascript/typing.js
+++ src/javascript/typing.js
@@ -4,60 +4,43 @@ import '../css/typing.css';
 
 
 
-const moulinette = function(text) {
-  let index = 0;
-  for (index = 0; index < text.length -1; index++) {
-    const keyCode = text.charCodeAt(index);
-    if (isNaN(keyCode)) {
-      console.log('nan !');
-    }
-  }
-  console.log('over : ' + index);
-}
-
-
-
-
-
 let text_all = window.localStorage.getItem('text_to_type');
-text_all = text_all.replace('«', '<');
-text_all = text_all.replace('»', '>');
-text_all = text_all.replace('&nbsp;', ' ');
-moulinette(text_all);
-
-
-// text_all = text_all.substring(6, text_all.length);
-
-
-let text_to_type = null;
+const regexp = /(«|»|–)/g;
+text_all = text_all.replace(regexp, ()=>{return '-'});
 
 const elm_text_all = document.querySelector('.text-all');
 const elm_text_to_type = document.querySelector('.typing__text');
 const elm_cursor = document.querySelector('.typing__cursor');
-const text_to_type_lgth = 110;
+const elm_success = document.querySelector('.success');
+const text_to_type_lgth = 80;
 let nb_chars_valid = 0;
 let char_to_type = null;
-
-
-
-
+let hit_state = true;
+const CURSOR_COLOR = {
+  true: "rgb(30, 102, 128)",
+  false: "rgb(163, 26, 33)"
+}
 
 
 const setTextToType = function(offset) {
-  text_to_type = text_all.substring(offset, text_to_type_lgth);
-  char_to_type = text_to_type.substring(0, 1);
-  if (isNaN(char_to_type.charCodeAt(0))) {
+  char_to_type = text_all.substring(offset, offset + 1);
+  // Security, we don't want to block the
+  // mechanism in case of unexpected char
+  if (isNaN(char_to_type.charCodeAt(0)) || char_to_type.charCodeAt(0) === 160 ) {
     console.warn('nan encountered');
     char_to_type = ' ';
   }
+  // Need to keep the right dimension of div cursor.
+  // Let's fill it with an invisible char
   if (char_to_type === ' ') {
-    elm_cursor.style.color= "rgb(30, 102, 128)";
-    elm_cursor.innerText = '_';
+    elm_cursor.style.color = CURSOR_COLOR.true
+    elm_cursor.innerText = '~';
   }
   else {
     elm_cursor.style.color= "white";
     elm_cursor.innerText = char_to_type;
   }
+
   console.log('code to type : ' + char_to_type.charCodeAt(0));
   console.log('char to type : ' + char_to_type);
   //elm_text_to_type.innerHTML = text_to_type.substring(1, text_to_type.length);
@@ -66,40 +49,33 @@ const setTextToType = function(offset) {
 
 // Au début
 elm_text_all.innerHTML = text_all;
+text_all = elm_text_all.innerText;
 setTextToType(0);
+console.log(text_all);
 
 
 // And when key pressed
 document.onkeypress = function (e) {
+  if (nb_chars_valid >= text_all.length) {
+    elm_success.style.display = 'block';
+    return;
+  }
   e.preventDefault();
   e = e || window.event;
   // console.log(String.fromCharCode(e.keyCode));
   if(char_to_type === String.fromCharCode(e.keyCode)) {
-    console.log('ok');
+    hit_state = true;
+    elm_cursor.style.background = CURSOR_COLOR.true;
     nb_chars_valid++;
     console.log('offset : ' + nb_chars_valid);
     setTextToType(nb_chars_valid);
+  } else {
+    console.log('WRONG');
+    hit_state = false;
+    elm_cursor.style.background = CURSOR_COLOR.false;
+    if (char_to_type === ' ') {
+      elm_cursor.style.color = CURSOR_COLOR.false;
+    }
   }
 };
-
-
-
-
-
-
-// const setTextToType = function(offset) {
-//   const new_char_to_type = text_all.substring(offset, 1);
-//   console.log('to type : ' + new_char_to_type);
-//   char_to_type = new_char_to_type;
-//   elm_cursor.innerHTML = new_char_to_type;
-//   let text_to_type = null;
-//   text_to_type = text_all.substring(offset, text_to_type_lgth);
-//   return text_to_type;
-// }
-
-// At beginning
-//const regex1 = /«/;
-// A l'arrache, à remplacer par expressions régulières !!! =====
-
-
 
