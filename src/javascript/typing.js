97921ddeb6adbb5447ec576c8a16e2f00005d344
anthonyl <anthony.lysiak@hotmail.com>
2020-04-05 19:38:32 +0100

    Le mechanisme de coloration du texte global est pas mal, mais ce n'est pas encore ce que je veux, je vais changer de mechanisme.

--- src/javascript/typing.js
+++ src/javascript/typing.js
@@ -13,11 +13,17 @@ const elm_text_to_type = document.querySelector('.typing__text');
 const elm_cursor = document.querySelector('.typing__cursor');
 const elm_success = document.querySelector('.success');
 const elm_typing_container = document.querySelector('.typing__container');
+const elm_textall_prev = document.querySelector('.textall__prev');
+const elm_textall_curr = document.querySelector('.textall__current');
+
 const text_to_type_lgth = 80;
 let nb_chars_valid = 0;
 let char_to_type = null;
 let hit_state = true;
 let cursor_position = 0;
+// Last char pressed is wrong = false;
+let current_state = true;
+
 const CURSOR_COLOR = {
   true: getComputedStyle(document.documentElement).getPropertyValue('--color-main'),
   false: getComputedStyle(document.documentElement).getPropertyValue('--color-error')
@@ -51,6 +57,7 @@ const setTextToType = function(offset) {
 
 // Au début
 elm_text_all.innerHTML = text_all;
+elm_textall_curr.innerHTML = text_all;
 text_all = elm_text_all.innerText;
 setTextToType(0);
 console.log(text_all);
@@ -67,6 +74,14 @@ document.onkeypress = function (e) {
   // console.log(String.fromCharCode(e.keyCode));
   // KEY PRESSED CORRECT  
   if(char_to_type === String.fromCharCode(e.keyCode)) {
+    if (!elm_textall_prev.classList.contains('textall__prev-cursorsuccess')) {
+      elm_textall_prev.classList.add('textall__prev-cursorsuccess');
+    }
+    if (elm_textall_prev.classList.contains('textall__prev-cursorerror')) {
+      elm_textall_prev.classList.remove('textall__prev-cursorerror');
+    }
+    colorizeCharInGlobalText(true, cursor_position);
+    current_state = true;
     cursor_position++;
     hit_state = true;
     elm_cursor.style.background = CURSOR_COLOR.true;
@@ -77,8 +92,17 @@ document.onkeypress = function (e) {
     setTextToType(nb_chars_valid);
   // KEY PRESSED INCORRECT
   } else {
+    if (elm_textall_prev.classList.contains('textall__prev-cursorsuccess')) {
+      elm_textall_prev.classList.remove('textall__prev-cursorsuccess');
+    }
+    if (!elm_textall_prev.classList.contains('textall__prev-cursorerror')) {
+      elm_textall_prev.classList.add('textall__prev-cursorerror');
+    }
     console.log('WRONG');
-    colorizeText(cursor_position);
+    if (current_state) {
+      colorizeCharInGlobalText(false);
+    }
+    current_state = false;
     hit_state = false;
     elm_cursor.style.background = CURSOR_COLOR.false;
     elm_typing_container.style.border = "4px solid var(--color-error)";
@@ -89,16 +113,15 @@ document.onkeypress = function (e) {
   }
 };
 
-const colorizeText = function(position) {
-  let text = elm_text_all.innerText;
-  let char = text.substring(position, position+1);
-  console.log(char);
-  char = "<span class='color-error'>" + char + "</span>";
-  text = text.substring(0, position) +
-          char +
-          text.substring(position+1, text.length);
-  elm_text_all.innerHTML = text;
-  
-  
-  
+const colorizeCharInGlobalText = function(success) {
+  if (!current_state && success) {
+    return;
+  }
+  let text = elm_textall_curr.innerText;
+  let char = text.substring(0, 1);
+  const class_name = success ? "color-success" : "color-error";
+  char = "<span class='"+class_name+"'>" + char + "</span>";
+  elm_textall_prev.innerHTML = elm_textall_prev.innerHTML + char;
+  text = text.substring(1, text.length);
+  elm_textall_curr.innerHTML = text;
 }
