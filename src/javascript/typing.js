92dfe8f0c90947c78dd4c5d3f2d2c17c4f668c29
anthonyl <anthony.lysiak@hotmail.com>
2020-04-05 20:43:09 +0100

    Le texte global du bas se comporte maintenant tres bien (colorations, curseur, etc...)

--- src/javascript/typing.js
+++ src/javascript/typing.js
@@ -1,33 +1,31 @@
+
 import '../css/main.css';
 import '../css/typing.css';
-
-
-
 
 let text_all = window.localStorage.getItem('text_to_type');
 const regexp = /(«|»|–)/g;
 text_all = text_all.replace(regexp, ()=>{return '-'});
 
-const elm_text_all = document.querySelector('.text-all');
 const elm_text_to_type = document.querySelector('.typing__text');
 const elm_cursor = document.querySelector('.typing__cursor');
 const elm_success = document.querySelector('.success');
 const elm_typing_container = document.querySelector('.typing__container');
 const elm_textall_prev = document.querySelector('.textall__prev');
 const elm_textall_curr = document.querySelector('.textall__current');
+const elm_textall_error = document.querySelector('.textall__error');
 
 const text_to_type_lgth = 80;
 let nb_chars_valid = 0;
 let char_to_type = null;
-let hit_state = true;
 let cursor_position = 0;
 // Last char pressed is wrong = false;
 let current_state = true;
-
 const CURSOR_COLOR = {
   true: getComputedStyle(document.documentElement).getPropertyValue('--color-main'),
   false: getComputedStyle(document.documentElement).getPropertyValue('--color-error')
 }
+
+
 
 
 const setTextToType = function(offset) {
@@ -55,35 +53,18 @@ const setTextToType = function(offset) {
   elm_text_to_type.innerHTML = text_all.substring(offset + 1, offset + text_to_type_lgth);
 }
 
-// Au début
-elm_text_all.innerHTML = text_all;
-elm_textall_curr.innerHTML = text_all;
-text_all = elm_text_all.innerText;
-setTextToType(0);
-console.log(text_all);
-
-
-// And when key pressed
-document.onkeypress = function (e) {
+// When key pressed
+const keyPressed = function(e) {
   if (nb_chars_valid >= text_all.length) {
     elm_success.style.display = 'block';
     return;
   }
   e.preventDefault();
   e = e || window.event;
-  // console.log(String.fromCharCode(e.keyCode));
   // KEY PRESSED CORRECT  
   if(char_to_type === String.fromCharCode(e.keyCode)) {
-    if (!elm_textall_prev.classList.contains('textall__prev-cursorsuccess')) {
-      elm_textall_prev.classList.add('textall__prev-cursorsuccess');
-    }
-    if (elm_textall_prev.classList.contains('textall__prev-cursorerror')) {
-      elm_textall_prev.classList.remove('textall__prev-cursorerror');
-    }
-    colorizeCharInGlobalText(true, cursor_position);
-    current_state = true;
+    colorizeCharInGlobalText(true);
     cursor_position++;
-    hit_state = true;
     elm_cursor.style.background = CURSOR_COLOR.true;
     elm_typing_container.style.border = "4px solid var(--color-main)";
     elm_text_to_type.style.color = "var(--color-main)";
@@ -92,18 +73,8 @@ document.onkeypress = function (e) {
     setTextToType(nb_chars_valid);
   // KEY PRESSED INCORRECT
   } else {
-    if (elm_textall_prev.classList.contains('textall__prev-cursorsuccess')) {
-      elm_textall_prev.classList.remove('textall__prev-cursorsuccess');
-    }
-    if (!elm_textall_prev.classList.contains('textall__prev-cursorerror')) {
-      elm_textall_prev.classList.add('textall__prev-cursorerror');
-    }
     console.log('WRONG');
-    if (current_state) {
-      colorizeCharInGlobalText(false);
-    }
-    current_state = false;
-    hit_state = false;
+    colorizeCharInGlobalText(false);
     elm_cursor.style.background = CURSOR_COLOR.false;
     elm_typing_container.style.border = "4px solid var(--color-error)";
     elm_text_to_type.style.color = "var(--color-error)";
@@ -114,14 +85,29 @@ document.onkeypress = function (e) {
 };
 
 const colorizeCharInGlobalText = function(success) {
-  if (!current_state && success) {
-    return;
+  if (success) {
+    elm_textall_error.innerText === '' ? moveCharInGlobalText(elm_textall_curr, elm_textall_prev) : 
+                                          moveCharInGlobalText(elm_textall_error, elm_textall_prev);
+  } else {
+    elm_textall_error.innerText === '' ? moveCharInGlobalText(elm_textall_curr, elm_textall_error) : 
+                                          '';
   }
-  let text = elm_textall_curr.innerText;
-  let char = text.substring(0, 1);
-  const class_name = success ? "color-success" : "color-error";
-  char = "<span class='"+class_name+"'>" + char + "</span>";
-  elm_textall_prev.innerHTML = elm_textall_prev.innerHTML + char;
-  text = text.substring(1, text.length);
-  elm_textall_curr.innerHTML = text;
 }
+
+const moveCharInGlobalText = function(elm_origin, elm_target) {
+  const text = elm_origin.innerText;
+  let char = text.substring(0, 1);
+  if (elm_target === elm_textall_prev) {
+    const class_name = elm_origin === elm_textall_curr ? "color-success" : "color-error";
+    char = `<span class='${class_name}'>${char}</span>`;
+  }
+  elm_target.innerHTML = elm_target.innerHTML + char;
+  elm_origin.innerHTML = text.substring(1, text.length);
+}
+
+// At beginning
+elm_textall_curr.innerHTML = text_all;
+text_all = elm_textall_curr.innerText;
+setTextToType(0);
+document.onkeypress = function(e) { keyPressed(e); }
+console.log(text_all);
