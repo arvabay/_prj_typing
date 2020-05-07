9a449755d4598cb6b5433af0a28034854b8eb552
anthonyl <anthony.lysiak@hotmail.com>
2020-05-07 14:46:14 +0100

    Upgrade: Debut de la modification des boutons + - dans le textPreviewer par une scrollbar

--- src/javascript/classes/TextPreviewer.js
+++ src/javascript/classes/TextPreviewer.js
@@ -4,13 +4,13 @@ import { addClass, removeClass } from '../modules/helpers'
  * 
  * @classdesc Build DOM representation of text supposed to be typed.
  * Build following blocks in a given root_element : button start, 
- * buttons minus / plus (reduce or increase text length), and text
+ * scroll minus / plus (reduce or increase text length), and text
  */
 export default class TextPreviewer {
 
   /**
    * 
-   * @param {Element} elm_root - The DOM Element in wich text and buttons will be append
+   * @param {Element} elm_root - The DOM Element in wich text and scroll will be append
    * @param {number} CHANGE_LENGTH - The number of chars added / removed at each change of text length 
    */
   constructor(elm_root, CHANGE_LENGTH) {
@@ -18,14 +18,20 @@ export default class TextPreviewer {
     this.CHANGE_LENGTH = CHANGE_LENGTH;
     this.current_text = ''; // used for text length changes and text dislay
     this.cut_position = null; // used for text length changes
-    this.elm_minus = document.createElement('a');
-    this.elm_minus.addEventListener('click', () => {
-      this.change_text_length(false, this.CHANGE_LENGTH);
-    });
-    this.elm_plus = document.createElement('a');
-    this.elm_plus.addEventListener('click', () => {
-      this.change_text_length(true, this.CHANGE_LENGTH);
-    });
+    
+        // this.elm_minus = document.createElement('a');
+        // this.elm_minus.addEventListener('click', () => {
+        //   this.change_text_length(false, this.CHANGE_LENGTH);
+        // });
+        // this.elm_plus = document.createElement('a');
+        // this.elm_plus.addEventListener('click', () => {
+        //   this.change_text_length(true, this.CHANGE_LENGTH);
+        // });
+        this.scroll_bar = document.createElement('div');
+        this.scroll_bar.classList.add('text__scrollbar');
+        this.scroll_cursor = document.createElement('div');
+        this.scroll_cursor.classList.add('text__scrollcursor');
+
     this.elm_start = document.createElement('a');
     this.elm_start.addEventListener('click', () => { 
       this.start();
@@ -49,15 +55,19 @@ export default class TextPreviewer {
     this.elm_start.innerText = 'Démarrer';
     const container = document.createElement('div');
     container.classList.add('text__container');
-    const buttons = document.createElement('div');
-    buttons.classList.add('text__buttons');
-    this.elm_minus.innerText = "-";
-    this.elm_minus.classList.add('text__button', 'enabled');
-    this.elm_plus.innerText = "+";
-    this.elm_plus.classList.add('text__button');
-    buttons.appendChild(this.elm_minus);
-    buttons.appendChild(this.elm_plus);
-    container.appendChild(buttons);
+    const scroll = document.createElement('div');
+    scroll.classList.add('text__buttons');
+
+        // this.elm_minus.innerText = "-";
+        // this.elm_minus.classList.add('text__button', 'enabled');
+        // this.elm_plus.innerText = "+";
+        // this.elm_plus.classList.add('text__button');
+        // scroll.appendChild(this.elm_minus);
+        // scroll.appendChild(this.elm_plus);
+    scroll.appendChild(this.scroll_bar);
+    scroll.appendChild(this.scroll_cursor);
+
+    container.appendChild(scroll);
     container.appendChild(this.elm_p);
     //---------------------------
     this.elm_p.classList.add('text__text');
@@ -92,13 +102,13 @@ export default class TextPreviewer {
 
   /**
    * 
-   * Result of a click button. Reduce / increase text length supposed to by typed. Disable minus / plus buttons if text is at its min / max size. Keep cut position to know where to apply next reduce / increase
+   * Result of a click button. Reduce / increase text length supposed to by typed. Disable minus / plus scroll if text is at its min / max size. Keep cut position to know where to apply next reduce / increase
    * @param {boolean} direction 
    * @returns {Void}
    * @private
    */
   change_text_length(direction) {
-    // prevent clicks if buttons aren't enabled
+    // prevent clicks if scroll aren't enabled
     if (!direction && !this.elm_minus.classList.contains('enabled') ||
           direction && !this.elm_plus.classList.contains('enabled') )
     {return;}
@@ -124,7 +134,7 @@ export default class TextPreviewer {
     this.elm_p.innerHTML = this.current_text.substring(
       0, this.cut_position);
   
-    // Enabling / disabling buttons
+    // Enabling / disabling scroll
     if (this.cut_position > 0) {
       addClass(this.elm_minus, 'enabled');
     }
