eaef4ad487bf27487ca4788ceb2061154c51ef89
anthonyl <anthony.lysiak@hotmail.com>
2020-05-09 20:09:06 +0100

    improvement: le fetcher Universalis renvoie maintenant beaucoup plus d'articles (si on ne trouve pas a la premiere recherche, on sonde eventuellement une URL donnee pour un article connexe)

--- src/javascript/modules/fetchers/universalis.js
+++ src/javascript/modules/fetchers/universalis.js
@@ -4,7 +4,11 @@ const url = "https://www.universalis.fr/encyclopedie/";
 
 export const universalis = async function(word) {
 
+
+
   return new Promise( (resolve, reject) => {
+
+
       fetch(url + word + '/').then( response => {
         // console.log(response.headers.get('status'));
         if (response.ok) {
@@ -13,22 +17,39 @@ export const universalis = async function(word) {
           reject('Erreur lors de la requête pour l\'url ' + this.url);
         }
       }).then( (html) => {
-        // console.log(html);
         var parser = new DOMParser();
         var doc = parser.parseFromString(html, "text/html");
-        // console.log(doc);
-        // const body = doc.getElementsByTagName('body');
         const corps = doc.getElementById('corps-prospect');
         if (corps) {
           const ps = corps.querySelector('p');
-          // elm_text.innerHTML = ps.innerHTML;
           resolve(ps.innerHTML);
         } else {
-          reject(not_found);
+          const next = doc.querySelector('.lirelasuite');
+          const str = next.href.split("/encyclopedie/").pop();
+          // console.log(url + str);
+          fetch(url + str).then( response => {
+            if (response.ok) {
+              return response.text();
+            } else {
+              reject('Erreur lors de la requête pour l\'url ' + this.url);
+            }
+          }).then( (html) => {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(html, "text/html");
+            const corps = doc.getElementById('corps-prospect');
+            if (corps) {
+              const ps = corps.querySelector('p');
+              resolve(ps.innerHTML);
+            } else {
+              
+              reject(not_found);
+            }
+          });
         }
       }).catch(e => {
         reject('error : ' + e);
       });
   });  
   
+
 }
